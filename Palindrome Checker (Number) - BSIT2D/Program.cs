5f4e4a43950c9eb878093ae55527e687a1200aa3
Kentruss <krajimeno@pcu.edu.ph>
2026-03-04 18:32:43 +0800

    second commit

---
+++ Palindrome Checker (Number) - BSIT2D/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Checker__Number____BSIT2D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string num = Console.ReadLine();
+
+            string reverse = "";
+
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                reverse += num[i];
+            }
+
+            if (num == reverse)
+            {
+                Console.WriteLine(num + " is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine(num + " is not a palindrome");
+            }
+            Console.ReadLine();
+        }
+    }
+}
